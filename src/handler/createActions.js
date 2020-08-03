d1adc2bf240944050a35f4f5648bf43ed91ad847
Panupon Wongman <gtblackhat@gmail.com>
2020-08-03 19:53:35 +0700

    fix api request

--- src/handler/createActions.js
+++ src/handler/createActions.js
@@ -5,10 +5,11 @@ import camelCase from 'lodash/camelCase'
 import { createPromiseAction } from './createPromiseAction'
 
 const prefix = 'Action'
+const prefixRequest = 'Request'
 
 export const createActions = (event, config) => {
-  if (isNil(config.createRequestCreator)) throw new Error('Required function `createRequestCreator`.')
-  if (typeof config.createRequestCreator !== 'function') throw new Error('`createRequestCreator` is not a function.')
+  if (isNil(config.request) || isEmpty(config.request)) throw new Error('Required apis request.')
+
   if (isEmpty(event)) throw new Error('Event is empty on create actions vuex.')
   if (!isArray(event)) throw new Error('Event is not array.')
 
@@ -16,14 +17,15 @@ export const createActions = (event, config) => {
   event.forEach((item) => {
     const { storeConfig: { action = '', eventName = '' } = {} } = item || {}
     if (isEmpty(eventName) || isNil(eventName)) throw new Error('Required key `eventName` in store config.')
+
     const actionName = isEmpty(action) || isNil(action) ? eventName : action
 
     object = {
       ...object,
-      [`${camelCase(actionName)}${prefix}`](store, payload) {
+      async [`${camelCase(actionName)}${prefix}`](store, payload) {
         return createPromiseAction({
           types: eventName,
-          promise: config.createRequestCreator(item, payload)
+          promise: config.request[`${eventName}${prefixRequest}`](item, payload)
         },
         store)
       }
