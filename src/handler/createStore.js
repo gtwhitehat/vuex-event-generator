a618077020b37dcbbca499b8836c59d2c7455c04
Panupon Wongman <gtblackhat@gmail.com>
2020-08-11 15:32:50 +0700

    function create store

---
+++ src/handler/createStore.js
@@ -0,0 +1,35 @@
+import isEmpty from 'lodash/isEmpty'
+import isArray from 'lodash/isArray'
+import isNil from 'lodash/isNil'
+
+import { createState } from './createState'
+import { createActions } from './createActions'
+import { createMutation } from './createMutation'
+import { createGetters } from './createGetters'
+
+export const createStore = (event, {
+  request, state = {}, actions = {}, mutations = {}, getters = {}
+}) => {
+  if (isNil(request)) console.error('Required field request in action vuex.')
+  if (isEmpty(event)) throw new Error('Event is empty on create actions vuex.')
+  if (!isArray(event)) throw new Error('Event is not array.')
+
+  return {
+    state: () => ({
+      ...createState(event),
+      ...state
+    }),
+    actions: {
+      ...createActions(event, { request }),
+      ...actions
+    },
+    mutations: {
+      ...createMutation(event),
+      ...mutations
+    },
+    getters: {
+      ...createGetters(event),
+      ...getters
+    }
+  }
+}
