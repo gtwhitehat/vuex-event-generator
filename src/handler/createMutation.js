cc1c0c0cc1e294e9c05c120b85d3ab28d8917e68
Panupon Wongman <gtblackhat@gmail.com>
2020-08-13 13:57:11 +0700

    fix api response

--- src/handler/createMutation.js
+++ src/handler/createMutation.js
@@ -15,7 +15,7 @@ export const createMutation = (event) => {
       }
     }
 
-    const { eventName = '', state = {} } = i || {}
+    const { eventName = '', state = {}, api = false } = i || {}
     if (isEmpty(eventName) || isNil(eventName)) console.error('Required key `eventName` in store config.')
 
     const stateName = isEmpty(state) || isNil(state.name) ? eventName : state.name
@@ -24,7 +24,8 @@ export const createMutation = (event) => {
         ...object,
         [`${eventName}_${s}`](state, payload = {}) {
           if (isNil(payload)) return
-          state[stateName] = payload
+          if (api) state[stateName].response = payload
+          else state[stateName] = payload
         }
       }
     })
